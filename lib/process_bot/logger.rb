11c578ad616c51125d620bb11978987208bd8130
kaspernj <k@spernj.org>
2022-10-18 16:55:11 +0000

    Only write to log if given in opts

--- lib/process_bot/logger.rb
+++ lib/process_bot/logger.rb
@@ -4,16 +4,22 @@ class ProcessBot::Logger
   def initialize(options:)
     @options = options
 
-    open_file
+    open_file if logging?
   end
 
   def log(output)
+    return unless logging?
+
     fp_log&.write(output)
     fp_log&.flush
   end
 
   def log_file_path
     options.fetch(:log_file_path)
+  end
+
+  def logging?
+    options.present?(:log_file_path
   end
 
   def open_file
