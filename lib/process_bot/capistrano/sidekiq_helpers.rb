3552dcaf87153834174fec165788cc160a5a7719
kaspernj <k@spernj.org>
2023-12-14 19:26:18 +0000

    Fixed lowercase

--- lib/process_bot/capistrano/sidekiq_helpers.rb
+++ lib/process_bot/capistrano/sidekiq_helpers.rb
@@ -35,17 +35,32 @@ module ProcessBot::Capistrano::SidekiqHelpers # rubocop:disable Metrics/ModuleLe
   def process_bot_command(process_bot_data, command)
     raise "No port in process bot data? #{process_bot_data}" unless process_bot_data["port"]
 
-    args = {command: command, port: process_bot_data.fetch("port")}
+    mode = "runner"
 
-    if command == :graceful && !fetch(:process_bot_wait_for_gracefully_stopped).nil?
-      args["wait-for-gracefully-stopped"] = fetch(:process_bot_wait_for_gracefully_stopped)
+    if mode == "runner"
+      args = {command: command, port: process_bot_data.fetch("port")}
+
+      if command == :graceful && !fetch(:process_bot_wait_for_gracefully_stopped).nil?
+        args["wait_for_gracefully_stopped"] = fetch(:process_bot_wait_for_gracefully_stopped)
+      end
+
+      escaped_args = JSON.generate(args).gsub("\"", "\\\"")
+      rails_runner_command = "require 'process_bot'; ProcessBot::Process.new(ProcessBot::Options.from_args(#{escaped_args})).execute!"
+
+      backend_command = "cd #{release_path} && " \
+        "#{SSHKit.config.command_map.prefix[:bundle].join(" ")} bundle exec rails runner \"#{rails_runner_command}\""
+    elsif mode == "exec"
+      backend_command = "cd #{release_path} && " \
+        "#{SSHKit.config.command_map.prefix[:bundle].join(" ")} bundle exec process_bot " \
+        "--command #{command} " \
+        "--port #{process_bot_data.fetch("port")}"
+
+      if command == :graceful && !fetch(:process_bot_wait_for_gracefully_stopped).nil?
+        backend_command << " --wait-for-gracefully-stopped #{fetch(:process_bot_wait_for_gracefully_stopped)}"
+      end
+    else
+      raise "Unknown mode: #{mode}"
     end
-
-    escaped_args = JSON.generate(args).gsub("\"", "\\\"")
-    rails_runner_command = "require 'process_bot'; ProcessBot::Process.new(ProcessBot::Options.from_args(#{escaped_args})).execute!"
-
-    backend_command = "cd #{release_path} && " \
-      "#{SSHKit.config.command_map.prefix[:bundle].join(" ")} bundle exec rails runner \"#{rails_runner_command}\""
 
     backend.execute backend_command
   end
