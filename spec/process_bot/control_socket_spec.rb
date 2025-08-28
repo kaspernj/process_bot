37e780e59f1cf1ef17ddca06677145f124a0f09b
kaspernj <kasper@diestoeckels.de>
2025-08-28 08:08:05 +0000

    Use difference ports than other specs

--- spec/process_bot/control_socket_spec.rb
+++ spec/process_bot/control_socket_spec.rb
@@ -2,19 +2,18 @@ require "spec_helper"
 
 describe ProcessBot::ControlSocket do
   it "increases the port if already in use" do
-    sleep 0.1 # Wait to give time to close any other TCP socket that might be in use from a previous spec
-
     options1 = ProcessBot::Options.new(handler: "sidekiq")
     process1 = ProcessBot::Process.new(options1)
-    control_socket1 = ProcessBot::ControlSocket.new(options: ProcessBot::Options.new(port: 7086), process: process1)
+    control_socket1 = ProcessBot::ControlSocket.new(options: ProcessBot::Options.new(port: 6086), process: process1)
     control_socket1.start
 
     options2 = ProcessBot::Options.new(handler: "sidekiq")
     process2 = ProcessBot::Process.new(options2)
-    control_socket2 = ProcessBot::ControlSocket.new(options: ProcessBot::Options.new(port: 7086), process: process2)
+    control_socket2 = ProcessBot::ControlSocket.new(options: ProcessBot::Options.new(port: 6086), process: process2)
     control_socket2.start
 
-    expect(control_socket2).to have_attributes(port: 7087)
+    expect(control_socket1).to have_attributes(port: 6086)
+    expect(control_socket2).to have_attributes(port: 6087)
   ensure
     control_socket1&.stop
     control_socket2&.stop
